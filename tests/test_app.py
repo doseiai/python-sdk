fb2557c3351be782371cf1c7c16a8d6e501b5524
Alw3ys <alw3ys@gmail.com>
2023-11-21 14:29:42 +0100

    0.0.11

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,7 @@
-import sys
-
 from dosei import Dosei
 import pytest
+
+from dosei.importer import import_from_string
 
 dosei = Dosei()
 
@@ -25,11 +25,8 @@ async def test_async_run_cron_job():
     await async_cron_job()
 
 
-def test_apply_app():
-    dosei.deploy()
-    assert len(dosei.cron_jobs) == 2
-
-
 def test_find_init():
     init_path = Dosei.find_init("./")
     assert f'{__name__}:dosei' == init_path
+    app: Dosei = import_from_string(init_path)
+    assert len(app.cron_jobs) == 2

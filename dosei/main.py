73d7b9992819bb52a47c93dde468466ba6a2cd7d
Alw3ys <alw3ys@gmail.com>
2023-11-22 14:26:37 +0100

    0.0.15

--- dosei/main.py
+++ dosei/main.py
@@ -1,3 +1,5 @@
+import sys
+
 import click
 
 from dosei import Dosei
@@ -12,11 +14,13 @@ def cli():
 @cli.command()
 @click.argument("func")
 def run(func):
+    sys.path.insert(0, "")
     Dosei.call_func(import_from_string(func))
 
 
 @cli.command()
 @click.argument("app")
 def export(app):
+    sys.path.insert(0, "")
     app: Dosei = import_from_string(app)
     return app.export()

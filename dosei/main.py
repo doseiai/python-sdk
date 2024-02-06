9ae3a9298339be5a574aef07c673c05007588a9b
Alw3ys <alw3ys@gmail.com>
2024-02-06 19:40:32 +0100

    export

--- dosei/main.py
+++ dosei/main.py
@@ -6,6 +6,8 @@ import click
 from dosei import Dosei
 from dosei.importer import import_from_string, ImportFromStringError
 
+dosei_config_init = "dosei_config:dosei"
+
 # Add current dir to PYTHONPATH
 sys.path.insert(0, "")
 
@@ -21,7 +23,7 @@ def run(func):
     if func:
         return Dosei.call_func(import_from_string(func))
     try:
-        app: Dosei = import_from_string("dosei_config:dosei")
+        app: Dosei = import_from_string(dosei_config_init)
     except ImportFromStringError:
         raise click.ClickException(f"Couldn't find a dosei_config.py file in \"{os.getcwd()}\"")
     if app.command is None:
@@ -32,7 +34,7 @@ def run(func):
 @cli.command()
 def dev():
     try:
-        app: Dosei = import_from_string("dosei_config:dosei")
+        app: Dosei = import_from_string(dosei_config_init)
     except ImportFromStringError:
         raise click.ClickException(f"Couldn't find a dosei_config.py file in \"{os.getcwd()}\"")
     if app.dev is None:
@@ -41,7 +43,9 @@ def dev():
 
 
 @cli.command()
-@click.argument("app")
-def export(app):
-    app: Dosei = import_from_string(app)
-    return app.export()
+def export():
+    try:
+        app: Dosei = import_from_string(dosei_config_init)
+        return app.export()
+    except ImportFromStringError:
+        raise click.ClickException(f"Couldn't find a dosei_config.py file in \"{os.getcwd()}\"")
